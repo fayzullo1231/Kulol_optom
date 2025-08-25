9a6cd145b283a01b012c6182e44d26a589619ce1
fayzullo1231 <fayzullodev@gmail.com>
2025-08-26 01:28:36 +0500

    start project

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,5 @@
-from xml.etree.ElementInclude import include
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
